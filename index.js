bf9b0b76b2fa1f1450d6bbb2be46006ea94adc73
FrodeBerg <bergstedt.frode@gmail.com>
2023-02-17 22:21:25 +0100

    Add working calculations and style changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     })    
 })
 
-
+const operations = ["+", "-", "*", "/"];
 let currentVariable = "";
 
 function add(num1, num2) {
@@ -21,7 +21,11 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        alert("Cant divide by 0 silly!");
+        return;
+    }
+    return Math.round(num1 / num2 * 100) / 100;
 }
 
 function operate(symbol, num1, num2) {
@@ -41,14 +45,72 @@ function operate(symbol, num1, num2) {
             operation = divide;
             break;
     }
-    result = operation(num1, num2);
+    result = operation(+num1, +num2);
     return result    
+}
+
+function solve() {
+
+    let length = currentVariable.length;
+
+    let num1 = null;
+    let symbolIndex = 0;
+    let symbol = "";
+    for (let i = 0; i < length; i++){
+        if ( i == length - 1){
+            let num2 = currentVariable.slice(symbolIndex + 1, i + 1);
+            if (num2.length == 0 || num1 == null) return;
+            num1 = operate(symbol, num1, num2);  
+        }
+        if (operations.includes(currentVariable[i])){
+            if (num1 === null) {
+                num1 = currentVariable.slice(0, i);
+            }else {
+                let num2 = currentVariable.slice(symbolIndex + 1, i);
+                console.log(symbol, num1, num2);
+                num1 = operate(symbol, num1, num2);
+            }
+            symbol = currentVariable[i];
+            symbolIndex = i;
+        }
+    }
+
+    currentVariable = num1.toString();
+    update();
 }
 
 function input(element){
     let text = element.innerText;
-    if (text == "Remove" || text == "Clear") return;
-    currentVariable += text;
-    let display = document.querySelector(".display");
+
+    if (text == "Clear") {
+        currentVariable = "";
+        update();
+        return;
+    };
+
+    if (text == "=") {
+        solve();
+        return;
+    }
+
+    if (text == "." && currentVariable.includes(".")){
+        return
+    }
+
+    if (text == "Remove") {
+        console.log(currentVariable)
+        currentVariable = currentVariable.slice(0, -1);
+    } else {
+        if (operations.includes(currentVariable[currentVariable.length - 1]) && operations.includes(text)) {
+            currentVariable = currentVariable.slice(0, -1)
+        }
+        currentVariable += text;
+    }
+    update();
+}
+
+function update(){
+    let display = document.querySelector("#currentVariable");
     display.innerHTML = currentVariable;
+
 }
